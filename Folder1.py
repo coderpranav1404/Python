7a210a119bb25ac1f98f824a725c4816fa6ac4a5
coderpranav1404 <kurhadepranav1404@gmail.com>
2026-08-26 19:02:45 +0530

    Add new Folder

---
+++ Folder1.py
@@ -0,0 +1,16 @@
+a=6
+b=7
+c=a+b*b#first multiply hote
+print(c)
+
+a=float(input("What is your no.")) #In output put the no.to proceed further
+str1 = "Web development helps to develop mind." 
+
+print(str1[4])
+print(str1)
+print(len(str1))
+print(type(c))
+
+print(str1.count("a"))
+
+#Strings are immutable means cannot replace
