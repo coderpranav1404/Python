9f9da9414db526cde49e68f4b610a8a7cff86b2f
coderpranav1404 <kurhadepranav1404@gmail.com>
2026-08-26 22:33:22 +0530

    added nested dictionary

--- file4.py
+++ file4.py
@@ -12,3 +12,13 @@ info["cgpa"]=100
 print(info)
 print(len(info))
 
+#Nested dictionary
+
+dict={ "exam":"12th Boards",
+      "Entrance":{"Jee":95} #Be careful about colon and=
+       }
+print(dict["exam"])
+dict["Entrance"]["Jee"]=99
+print(dict)
+
+
