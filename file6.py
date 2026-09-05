d317b692045c44072fec139a5c94346ab8c41692
Pranav <kurhadepranav1404@gmail.com>
2026-09-05 11:05:50 +0530

    Update file6.py

---
+++ file6.py
@@ -0,0 +1,45 @@
+#function
+
+def calc_avg(a,b,c):
+  avg=(a+b+c)/3
+  print(avg)
+  return avg
+
+
+  
+calc_avg(3,4,5)
+
+def calc_pro(a=1,b=8):
+  pro=(a*b)
+  print(pro)
+  return pro
+calc_pro()
+
+
+
+str="Pranav"
+
+def calc_len(str):
+  print(len(str))
+  return len
+
+calc_len(str)
+
+
+num=[1,3,5]
+
+def cal_list(num):
+  print(list(num))
+  return list
+
+cal_list(num)
+  
+#To calculate factorial
+def calc_fact(n):
+  fact=1
+  for i in range(1,n+1):
+    fact*=i
+  print(fact)
+  return fact
+    
+calc_fact(4)
